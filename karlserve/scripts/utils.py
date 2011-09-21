7af6bee07eb12b9d98b14d5dc41fde17013e670c
Chris Rossi <chris@archimedeanco.com>
2011-09-21 10:52:36 -0400

    parse_dsn

--- karlserve/scripts/utils.py
+++ karlserve/scripts/utils.py
@@ -51,3 +51,58 @@ def shell_script(f):
         except subprocess.CalledProcessError, e:
             sys.exit(1)
     return wrapper
+
+
+def parse_dsn(dsn):
+    """ Parse a postgresql dsn using a finite state machine.
+    """
+    SCANNING = 0
+    IN_NAME = 1
+    IN_VALUE = 2
+    quote_chars = "'"""
+
+    state = SCANNING
+    token_begin = None
+    name = None
+    quote_char = None
+    length = len(dsn)
+    data = {}
+
+    i = 0
+    while i < length:
+        ch = dsn[i]
+        if state == SCANNING:
+            if not ch.isspace():
+                token_begin = i
+                state = IN_NAME
+            else:
+                i += 1
+
+        elif state == IN_NAME:
+            if ch == '=':
+                name = dsn[token_begin:i]
+                i += 1
+                next_ch = dsn[i]
+                if next_ch in quote_chars:
+                    quote_char = next_ch
+                    i += 1
+                token_begin = i
+                state = IN_VALUE
+            else:
+                i += 1
+
+        elif state == IN_VALUE:
+            end = ch.isspace() if not quote_char else ch == quote_char
+            if end:
+                value = dsn[token_begin:i]
+                data[name] = value
+                name = value = quote_char = None
+                state = SCANNING
+            i += 1
+
+    assert state in (SCANNING, IN_VALUE)
+    if state == IN_VALUE:
+        value = dsn[token_begin:]
+        data[name] = value
+
+    return data
