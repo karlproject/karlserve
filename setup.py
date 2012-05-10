b01b796e4c37cd005bc9b8a2e4e5f0b6b8444994
Chris Rossi <chris@archimedeanco.com>
2012-05-10 16:35:57 -0400

    Prep for release.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-__version__ = '1.13'
+__version__ = '1.14'
 
 import os
 
